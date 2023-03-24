b7c964d575698fab005e83ff933f4c265fd9c998
Ariel Lin <smsllbear1600@gmail.com>
2023-03-24 15:45:11 +0800

    feat: move starter theme [SPMVP-4928]

---
+++ templates/resources/author.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+const author = setupAuthorPage()
+</script>
+
+<template>
+  <!-- header (shared) -->
+  <section class="m-4">header</section>
+
+  <!-- navigation (shared) -->
+  <section class="m-4">navigation</section>
+
+  <section class="m-4">
+    <picture>
+      <img :alt="author.full_name" :src="author.avatar" />
+    </picture>
+
+    <h1>{{ author.full_name }}</h1>
+
+    <p>{{ author.bio }}</p>
+
+    <div>
+      {{ author.socials }}
+    </div>
+  </section>
+</template>
