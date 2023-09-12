6b9031a752be61cc2dea65c4fceeeb5b21a21ccc
Sid Chang <37400982+SidStraw@users.noreply.github.com>
2023-09-12 17:43:25 +0800

    chore: [SPMVP-6294] add example seo config to starter theme (#6)

--- templates/resources/desk.vue
+++ templates/resources/desk.vue
@@ -1,11 +1,22 @@
 <script lang="ts" setup>
+import type { MetaFlatInput } from 'zhead'
 const desk = setupDeskPage()
 
-useHead(() => {
-  return {
-    title: `All articles under ${desk.value.name}`,
-  }
-})
+// override seo meta for each desk
+const seoData: Record<string, MetaFlatInput & { title?: string | null | undefined }> = {
+  'Drag a card here': {
+    title: 'Drag a card here',
+    description: 'Drag a card here Description',
+  },
+  'Delete me': {
+    title: 'Delete me',
+    description: 'Delete me Description',
+  },
+}
+useSeoMeta(() => ({
+  title: `All articles under ${desk.value.name}`, // can set default value here
+  ...seoData[desk.value.name],
+}))
 </script>
 
 <template>
